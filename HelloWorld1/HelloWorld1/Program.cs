843df756443aa2c9c9c7f750e884eb2c57ad7233
Zeynep Ceyhan <104215922+zeynepnceyhan@users.noreply.github.com>
2022-08-01 12:29:13 +0300

    Hello world code in C#
    Internship

---
+++ HelloWorld1/HelloWorld1/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
